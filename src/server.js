f7565ee7d07ad017bae9f7067960cfbef4cc186c
Oleksandr08 <is4uk2011@icloud.com>
2025-12-02 11:14:37 +0200

    Home work 4

--- src/server.js
+++ src/server.js
@@ -1,32 +1,52 @@
-// src/server.js
-
 import express from 'express';
 import cors from 'cors';
-
-import helmet from 'helmet';
 import 'dotenv/config';
+import helmet from 'helmet';
 import { connectMongoDB } from './db/connectMongoDB.js';
-
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-
-import notesRouters from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import notesRoutes from './routes/notesRoutes.js';
 import { errors } from 'celebrate';
+import cookieParser from 'cookie-parser';
 
 const app = express();
-const PORT = process.env.PORT ?? 3030;
+const PORT = process.env.PORT ?? 3000;
 
 app.use(logger);
-app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(notesRouters);
-app.use(errors());
-app.use(notFoundHandler);
-app.use(errorHandler);
-await connectMongoDB();
+app.use(express.json());
+app.use(cookieParser());
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use(authRoutes);
+app.use(notesRoutes);
+
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Welcome to Notes API 🚀',
+    availableRoutes: ['/notes', '/notes/:noteId'],
+  });
 });
+
+app.use(notFoundHandler);
+
+app.use(errors());
+
+app.use(errorHandler);
+
+const startServer = async () => {
+  try {
+    await connectMongoDB();
+
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('Failed to start server:', err);
+    process.exit(1);
+  }
+};
+
+startServer();
